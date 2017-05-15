93f6955433ca0e689e52192430c20f9c489dff98
FienSpriet <fien.spriet.t4882@student.hogent.be>
2017-05-15 21:18:47 +0200

    attributen toegevoegd aan paar

--- src/domein/Paar.java
+++ src/domein/Paar.java
@@ -10,5 +10,27 @@ package domein;
  * @author fiens
  */
 public class Paar {
-    
+    private String woord;
+    private String pad;
+
+    public Paar(String woord, String pad) {
+        this.woord = woord;
+        this.pad = pad;
+    }
+
+    public String getWoord() {
+        return woord;
+    }
+
+    public void setWoord(String woord) {
+        this.woord = woord;
+    }
+
+    public String getPad() {
+        return pad;
+    }
+
+    public void setPad(String pad) {
+        this.pad = pad;
+    }
 }
