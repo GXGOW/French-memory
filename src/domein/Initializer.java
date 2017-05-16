c86f04874690d9a86210bac9f2e9177d291a1a4f
FienSpriet <fien.spriet.t4882@student.hogent.be>
2017-05-16 21:53:40 +0200

    franse woorden ingevuld

--- src/domein/Initializer.java
+++ src/domein/Initializer.java
@@ -19,8 +19,42 @@ public class Initializer {
 
     public Initializer() {
         paren  = Arrays.asList(new Paar[]{
-            new Paar("woord1", "pad1"),
-            new Paar("woord2", "pad2")
+            new Paar("un arbre", ""),
+            new Paar("un garçon", ""),
+            new Paar("une fille", ""),
+            new Paar("une pomme", ""),
+            new Paar("une maison", ""),
+            new Paar("une balle", ""),
+            new Paar("un chien", ""),
+            new Paar("un chat", ""),
+            new Paar("un cheval", ""),
+            new Paar("un chaise", ""),
+            new Paar("une étoile", ""),
+            new Paar("un manteau", ""),
+            new Paar("un bateau", ""),
+            new Paar("la mer", ""),
+            new Paar("une poupée", ""),
+            new Paar("un livre", ""),
+            new Paar("un poisson", ""),
+            new Paar("le soleil", ""),
+            new Paar("une glace", ""),
+            new Paar("une fleur", ""),
+            new Paar("une vache", ""),
+            new Paar("une table", ""),
+            new Paar("un ours", ""),
+            new Paar("une chaussette", ""),
+            new Paar("un verre", ""),
+            new Paar("rouge", ""),
+            new Paar("vert", ""),
+            new Paar("jaune", ""),
+            new Paar("bleu", ""),
+            new Paar("un papier", ""),
+            new Paar("un pendule", ""),
+            new Paar("un cochon", ""),
+            new Paar("une clef", ""),
+            new Paar("une armoire", ""),
+            new Paar("une chaussure", ""),
+            new Paar("une voiture", "")
         });
     }
 
